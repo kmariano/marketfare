d261bec3e805a2388457f3fd0322f822cee0bbf1
koshin <koshin.mariano@gmail.com>
2013-06-01 18:15:51 -0700

    now serving client side file statically

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express')
+  , application_root = __dirname
   , routes = require('./routes')
   , request = require('request')
   , ejs    = require('ejs')
@@ -22,16 +23,19 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(application_root, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.post('/market/find', market.search );
+app.get('/', function( req, res){
 
+   res.sendfile(__dirname + '/public/views/main.html');
+});
+
+app.post('/market/find', market.search );
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
